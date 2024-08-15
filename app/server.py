9e17a2ef7e0bdef9dfccb39c64ae595d2b7f4143
lasarocks <134645284+lasarocks@users.noreply.github.com>
2024-08-15 16:08:46 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,27 +1,17 @@
 import os
-from app.core.g1 import dbPersistente
+
 
 arq_cred = os.getenv("Gcredentials", None)
-dd = None
 
-if arq_cred:
-    print(arq_cred)
-    with open('teste.json', 'w') as f_cred:
-        f_cred.write(arq_cred)
-    dd = dbPersistente(path_file_auth=None, path_file_token='teste.json')
-    arqs_rmt = dd.list()
-    if arqs_rmt:
-        arq_db = arqs_rmt[0]
-        if os.path.isfile(arq_db['title']):
-            print(f'deletando antigo')
-            av = os.remove(arq_db['title'])
-        arq_db.GetContentFile(arq_db['title'])
-        print(f'baixando novo')
-        if os.path.isfile(arq_db['title']):
-            print(f'novo OK')
-    print(f'chego refresh status == {dd.inst_google_auth.credentials.access_token_expired}')
-else:
-    print(f'sem arq_cred')
+from app.core.database import(
+    DB_NAME,
+    quickDbFreeze
+)
+
+quicker = quickDbFreeze(arq_cred)
+quicker.update_local_db(DB_NAME)
+
+
 
 
 import urllib.parse
@@ -42,7 +32,7 @@ from app.core.database import(
     SessionLocal,
     engine,
     Base,
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
 )
 
 
@@ -53,13 +43,13 @@ from app.api.base import api_router
 from app.exceptions import errorHandler
 
 
-#Base.metadata.create_all(bind=engine)
+
 Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 app.path_database = os.path.abspath(urllib.parse.urlparse(SQLALCHEMY_DATABASE_URL).path[1:])
-app.inst_gdrive = dd
+app.inst_gdrive = quicker.inst_db
 
 app.add_middleware(
     CORSMiddleware,
@@ -78,8 +68,11 @@ async def startup_event():
 def shutdown_event():
     print(f'tamoooo indo de offf')
     print(f'vamo tenta upar...')
-    dd.upload(arq_db['title'])
-    print(f'acho que upou....')
+    av = quicker.update_remote_db(DB_PATH=app.path_database, DB_NAME=DB_NAME)
+    if av:
+        print(f'upou av == True')
+    else:
+        print(f'av != true')
 
 
 
