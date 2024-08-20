f894adf8561bc0a9dc32028f9633e1019c7ccdcf
lasarocks <134645284+lasarocks@users.noreply.github.com>
2024-08-20 01:22:55 -0300

    Add files via upload

--- app/models/schemas/sbinna.py
+++ app/models/schemas/sbinna.py
@@ -17,7 +17,8 @@ class binIssuersAdd(baseSchema):
 
 
 class binIssuersAddResponse(binIssuersAdd):
-    id: UUID
+    #id: UUID
+    id: int
     date_created: datetime
 
 
@@ -27,14 +28,92 @@ class binIssuersListAllResponse(baseSchema):
 
 
 class binnerAdd(baseSchema):
-    id_issuer: str
+    #id_issuer: str
+    id_issuer: int
     card_bin: str
+    card_type: Optional[str] = None
+    card_category: Optional[str] = None
+    product_category: Optional[str] = None
+    cvc_mandatory: Optional[bool] = None
+
+
+
+
+
+class CardNetworkAdd(baseSchema):
+    name: str
+    description: Optional[str] = None
+
+
+class CardNetworkAddResponse(CardNetworkAdd):
+    #id: UUID
+    id: int
+    date_created: datetime
+
+
+class CardNetworkListAllResponse(baseSchema):
+    data: List[CardNetworkAddResponse]
+
+
+
+
+class CardProductAdd(baseSchema):
+    name: str
+    description: Optional[str] = None
+
+
+class CardProductAddResponse(CardProductAdd):
+    #id: UUID
+    id: int
+    date_created: datetime
+
+
+class CardProductListAllResponse(baseSchema):
+    data: List[CardProductAddResponse]
+
+
+
+
+
+class binNetworkAdd(baseSchema):
+    #id_bin: str
+    id_bin: int
+    #id_network: str
+    id_network: int
+
+
+class binNetworkAddResponse(binNetworkAdd):
+    #id: UUID
+    id: int
+    date_created: datetime
+    networks: CardNetworkAddResponse
+
+
+
+class binProductAdd(baseSchema):
+    #id_bin: str
+    id_bin: int
+    #id_product: str
+    id_product: int
+
+
+class binProductAddResponse(binProductAdd):
+    #id: UUID
+    id: int
+    date_created: datetime
+    products: CardProductAddResponse
+
+
+
 
 
 class binnerAddResponse(binnerAdd):
-    id: UUID
+    #id: UUID
+    id: int
     date_created: datetime
     binIssuers: binIssuersAddResponse
+    bin_networks: List[binNetworkAddResponse]
+    products: List[binProductAddResponse]
 
 
 class binnerListAllResponse(baseSchema):
