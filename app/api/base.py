ed0711339bb0648fe4653b01def18e2a143d61f9
lasarocks <134645284+lasarocks@users.noreply.github.com>
2024-09-12 23:33:26 +0000

    Add files via upload

--- app/api/base.py
+++ app/api/base.py
@@ -5,6 +5,7 @@ from app.api.routes import route_transactions
 from app.api.routes import route_gateways
 from app.api.routes import route_cards
 from app.api.routes import route_bin
+from app.api.routes import route_api
 
 
 api_router = APIRouter()
@@ -17,3 +18,7 @@ api_router.include_router(route_gateways.router, prefix="/gateways", tags=["gate
 api_router.include_router(route_cards.router, prefix="/cards", tags=["cards"])
 
 api_router.include_router(route_bin.router, prefix="/bin", tags=["bin"])
+
+
+
+api_router.include_router(route_api.router, prefix="/service", tags=["service"])
